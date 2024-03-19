785561420c8f5ecbb12c8d7465e85a969de9d5f2
Willian Rios <williangrios@yahoo.com.br>
2024-03-19 09:31:09 -0300

    chore: frontend project created

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary-color-strong": "#ae55c7",
+        "primary-color-medium": "#ab63ce",
+        "primary-color-light": "#e7cef2",
+        "secondary-color-strong": "#bf5622",
+        "secondary-color-medium": "#faa01a",
+        "secondary-color-light": "#fdc428"
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
