69ffbb976184aa3c84baa18e9043f62bb7a83fc4
Willian Rios <williangrios@yahoo.com.br>
2024-03-20 07:08:15 -0300

    refactor: change service

--- frontend/src/app/services/HandlingErrorService.ts
+++ frontend/src/app/services/HandlingErrorService.ts
@@ -1,9 +1,12 @@
 export default class HandlingErrorService {
     static getShortError (message: string): string {
         if (message === 'Connector not connected.'){
-            return 'Conectar wallet'
+            return 'Primeiro você deve fazer login'
         }
         const errorMessage = message.replace(/^.*?:\s*/, '');
+        if (errorMessage === 'User rejected the request.') {
+            return 'Cancelado pelo usuário'
+        }
         return errorMessage;
     }
 }
