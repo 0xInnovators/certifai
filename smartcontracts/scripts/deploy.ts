1a5c01a6123e5d7fefc5749d738432c1adbc7b36
Willian Rios <williangrios@yahoo.com.br>
2024-03-20 07:05:31 -0300

    feat: add deploy nft smart contract

--- smartcontracts/scripts/deploy.ts
+++ smartcontracts/scripts/deploy.ts
@@ -4,6 +4,10 @@ async function main() {
   const AM = await hre.ethers.getContractFactory("AcademicManager");
   const am = await AM.deploy();
   console.log(`AcademicManager deployed to ${await am.getAddress()}`);
+
+  const CERTIFAI = await hre.ethers.getContractFactory("CERTIFAI");
+  const certifAI = await CERTIFAI.deploy(await am.getAddress());
+  console.log(`CERTIFAI deployed to ${await certifAI.getAddress()}`);
 }
 
 main().catch((error) => {
